bfd539b87ffc49c1caa656928b9eed7fdffd0666
xojan0120 <xojan0120@gmail.com>
2019-03-07 10:21:52 +0900

    [Add] テストコード

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import numeral from "numeral";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //numeral.local = 'ja';
+  }
+
+  hoge = () => {
+    let price = numeral(1000).format('0,0');
+    let old = numeral(price)
+    console.log(price)
+    console.log(old.value())
+  }
+
   render() {
     return (
       <div className="App">
+        {this.hoge()}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
